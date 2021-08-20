58eb63acdc525e816d5f824d86b427ba71ecec42
Israel Sousa <me@isaelsousa.com.br>
2021-08-20 19:07:26 -0300

    shore: adding bootstrap and preparing first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import styles from '../styles/Form.module.css'
 
 export default function Form() {
-  return <h1>Form</h1>
+  return (
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-label">Email address</label>
+        <input type="email" className="form-control" placeholder="name@example.com" />
+      </div>
+      <div class="mb-3">
+        <label className="form-label">Example textarea</label>
+        <textarea className="form-control" rows="3"></textarea>
+      </div>
+    </div>
+  )
 }
